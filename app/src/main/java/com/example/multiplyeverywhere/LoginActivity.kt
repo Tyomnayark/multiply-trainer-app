5956218363d73eda81dbef94359886e4bbeed88e
Elena Tolmacheva <tyomnayark@gmail.com>
2023-09-02 23:57:03 +0600

    fix authtorization

---
+++ app/src/main/java/com/example/multiplyeverywhere/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.example.multiplyeverywhere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.multiplyeverywhere.database.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val textArea = findViewById<EditText>(R.id.user_name)
+        val singInButton = findViewById<Button>(R.id.sign_in_button)
+
+
+        singInButton.setOnClickListener {
+            while (true) {
+                val name = textArea.text.toString().trim()
+                if (name != "") {
+                    val user = User(name, 0)
+                    val db = DataBaseController(this, null)
+                    db.addUser(user)
+                    val preferences = SharedPreferencesHelper(this)
+                    preferences.setUserName(name)
+                    val greetingMessage = getString(R.string.Greetings) + " " + name + "!"
+                    Toast.makeText(this, greetingMessage, Toast.LENGTH_SHORT).show()
+                    this.finish()
+                } else {
+                    Toast.makeText(this, R.string.login_error, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/multiplyeverywhere/database/DataBaseController.kt
@@ -0,0 +1,59 @@
+
+package com.example.multiplyeverywhere.database
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.multiplyeverywhere.User
+
+class DataBaseController (val context: Context , val factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper (context, "users", factory, 1){
+    override fun onCreate(db: SQLiteDatabase?) {
+        val query = "CREATE TABLE users (id INT PRIMARY KEY, name STRING, level INT)"
+        db!!.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS users")
+        onCreate(db)
+    }
+    fun addUser(user: User){
+        val values  = ContentValues()
+        values.put("name", user.name)
+        values.put("level", user.level)
+        val db = this.writableDatabase
+        db.insert("users", null, values)
+    }
+    fun doesDatabaseExist(context: Context, dbName: String): Boolean {
+        val dbFile = context.getDatabasePath(dbName)
+        return dbFile.exists()
+    }
+
+    @SuppressLint("Range")
+    fun getUserByName(userName: String): User? {
+        val db = this.readableDatabase
+        val cursor = db.query(
+            "users",
+            arrayOf("id", "name", "level"),
+            "name = ?",
+            arrayOf(userName.toString()),
+            null, null, null, null
+        )
+
+        var user: User? = null
+
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                val name = cursor.getString(cursor.getColumnIndex("name"))
+                val level = cursor.getInt(cursor.getColumnIndex("level"))
+                user = User( name, level)
+            }
+            cursor.close()
+        }
+
+        return user
+    }
+
+}
