947f9052b2ac868fc098b9d5970476883988c881
tyomnayark <tyomnayark@gmail.com>
2023-09-13 22:01:03 +0600

    add win activity back

--- app/src/main/java/com/example/multiplyeverywhere/ui/trainer/WInActivity.kt
+++ app/src/main/java/com/example/multiplyeverywhere/ui/trainer/WInActivity.kt
@@ -20,6 +20,7 @@ class WInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_win)
+        supportActionBar?.hide()
 
         val rounds = intent.getIntExtra("rounds", 0)
         val lives = intent.getIntExtra("lives", 0)
@@ -29,7 +30,7 @@ class WInActivity : AppCompatActivity() {
         val user = db.getUserByName(userName)
 
 
-        val points = (10-rounds-2+lives) * 50 + lives * 50 + user!!.level
+        val points = (rounds-2+lives) * 50 + lives * 50 + user!!.level
 
         val startProgress = countProgress(user!!.level)
         val endProgress = countProgress(points)
@@ -41,29 +42,41 @@ class WInActivity : AppCompatActivity() {
         levelText.setText(startLevel.toString())
 
         val pointsText = findViewById<TextView>(R.id.points_text)
-        pointsText.setText("+ "+((10-rounds-2+lives) * 100 + lives * 50 ).toString()+" "+ getString(R.string.points_text))
+        val newPointsText = "+ " + (( rounds - 2 + lives) * 100 + lives * 50).toString() + " " + getString(R.string.points_text)
+        pointsText.text = newPointsText
+
+        val scalePointsAnimator = ValueAnimator.ofFloat(1f, 1.2f)
+        scalePointsAnimator.duration = 500
+        scalePointsAnimator.repeatCount = 1
+        scalePointsAnimator.repeatMode = ValueAnimator.REVERSE
+
+        scalePointsAnimator.addUpdateListener { animation ->
+            val scale = animation.animatedValue as Float
+            pointsText.scaleX = scale
+            pointsText.scaleY = scale
+        }
+        val pointsAnimatorSet = AnimatorSet()
+        pointsAnimatorSet.playSequentially(scalePointsAnimator)
+        pointsAnimatorSet.start()
+
 
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
 
         if (startLevel!=finalLevel) {
 
-            var animator = ObjectAnimator.ofInt(progressBar, "progress", startProgress, 100)
-            animator.interpolator = AccelerateDecelerateInterpolator()
-            animator.duration = 1000L
-            animator.start()
             val levelText = findViewById<TextView>(R.id.level_text)
             val newText = finalLevel.toString()
 
-            val scaleAnimation = ScaleAnimation(
-                1f, 1.2f,
-                1f, 1.2f,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF, 0.5f
-            )
+            val scaleAnimator = ValueAnimator.ofFloat(1f, 1.5f)
+            scaleAnimator.duration = 500
+            scaleAnimator.repeatCount = 1
+            scaleAnimator.repeatMode = ValueAnimator.REVERSE
 
-            scaleAnimation.duration = 500
-            scaleAnimation.repeatCount = 1
-            scaleAnimation.repeatMode = Animation.REVERSE
+            scaleAnimator.addUpdateListener { animation ->
+                val scale = animation.animatedValue as Float
+                levelText.scaleX = scale
+                levelText.scaleY = scale
+            }
 
             val textChangeAnimator = ValueAnimator.ofObject(TypeEvaluator<String> { fraction, startValue, endValue ->
                 val start = startValue.toString()
@@ -74,7 +87,7 @@ class WInActivity : AppCompatActivity() {
                 for (i in 0 until length) {
                     val cStart = if (i < start.length) start[i] else ' '
                     val cEnd = if (i < end.length) end[i] else ' '
-                    result.append((cStart + (cEnd - cStart) * fraction).toChar())
+                    result.append((cStart + ((cEnd - cStart) * fraction).toInt()).toChar())
                 }
                 result.toString()
             }, levelText.text, newText)
@@ -87,18 +100,23 @@ class WInActivity : AppCompatActivity() {
             }
 
             val animatorSet = AnimatorSet()
-            animatorSet.playSequentially(scaleAnimation, textChangeAnimator)
+            animatorSet.playSequentially(scaleAnimator, textChangeAnimator)
             animatorSet.start()
+
+            var animator = ObjectAnimator.ofInt(progressBar, "progress", startProgress, 100)
+            animator.interpolator = AccelerateDecelerateInterpolator()
+            animator.duration = (100-startProgress)*100L
+            animator.start()
 
             val progressAnimator = ObjectAnimator.ofInt(progressBar, "progress", 0, endProgress)
             progressAnimator.interpolator = AccelerateDecelerateInterpolator()
-            progressAnimator.duration = 1000L
+            progressAnimator.duration = endProgress*100L
             progressAnimator.start()
 
         }else {
             val animator = ObjectAnimator.ofInt(progressBar, "progress", startProgress, endProgress)
             animator.interpolator = AccelerateDecelerateInterpolator()
-            animator.duration = 1000L
+            animator.duration = (endProgress-startProgress)*100L
             animator.start()
         }
 
