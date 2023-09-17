c53bb6dc2a54115e74e182dd641c711173bd4427
tyomnayark <tyomnayark@gmail.com>
2023-09-18 03:14:38 +0600

    fix kazakh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
-    implementation ("com.google.android.gms:play-services-ads:20.5.0")
+
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
